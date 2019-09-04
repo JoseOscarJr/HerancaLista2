0d1ac003039f56c2ef019ff9cf3723f7a353954d
JoseOscarJr <53666786+JoseOscarJr@users.noreply.github.com>
2019-09-04 20:31:27 -0300

    Add files via upload

---
+++ Caminhao.java
@@ -0,0 +1,14 @@
+package henranca2;
+
+public class Caminhao extends Veiculo {
+	private int eixos;
+
+	public int getEixos() {
+		return eixos;
+	}
+
+	public void setEixos(int eixos) {
+		this.eixos = eixos;
+	}
+	
+}
---
+++ Veiculo.java
@@ -0,0 +1,14 @@
+package henranca2;
+
+public class Veiculo {
+	private boolean motor;
+
+	public boolean isMotor() {
+		return motor;
+	}
+
+	public void setMotor(boolean motor) {
+		this.motor = motor;
+	}
+	
+}
